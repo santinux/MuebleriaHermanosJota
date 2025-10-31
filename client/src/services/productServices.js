e9cb7c56de4268a12b9cf9b2f56ff3a2b80b6d70
Valentina <valentinamartel05@gmail.com>
2025-10-31 20:47:11 -0300

    Actualizado productServices.js agrego para la creacion de productos

--- client/src/services/productServices.js
+++ client/src/services/productServices.js
@@ -70,3 +70,27 @@ export const searchProducts = async (searchTerm) => {
         throw error;
     }
 };
+
+export const createProduct = async (productData) => {
+    try {
+        const response = await fetch(`${BASE_URL}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(productData)
+        });
+
+        if (!response.ok) {
+            throw new Error('Error al crear el producto');
+        }
+        const { success, data } = await response.json();
+        if (!success) {
+            throw new Error('El servidor no pudo crear el producto');
+        }
+        return data;
+    } catch (error) {
+        console.error('Error creando producto:', error);
+        throw error;
+    }
+};
